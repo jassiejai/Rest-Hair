571a4982074295bf1233cac89432086793571d07
Jassie <jasminehart94@gmail.com>
2022-03-29 04:29:45 -0700

    Creating Spring application

---
+++ src/main/java/com/example/resthair/RestHairApplication.java
@@ -0,0 +1,13 @@
+package com.example.resthair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestHairApplication.class, args);
+    }
+
+}
