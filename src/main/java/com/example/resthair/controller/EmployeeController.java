a7d1bde780ee7b58fc86628bb9181fb8898f0e14
Jassie <jasminehart94@gmail.com>
2022-03-29 15:10:59 -0700

    Creating Service

---
+++ src/main/java/com/example/resthair/controller/EmployeeController.java
@@ -0,0 +1,4 @@
+package com.example.resthair.controller;
+
+public class EmployeeController {
+}
--- src/main/java/com/example/resthair/service/EmployeeService.java
+++ src/main/java/com/example/resthair/service/EmployeeService.java
@@ -1,4 +1,37 @@
 package com.example.resthair.service;
 
+
+import com.example.resthair.model.Employee;
+import com.example.resthair.repository.EmployeeRepostiory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepostiory employeeRepostiory;
+
+    public void setEmployeeRepostiory(EmployeeRepostiory employeeRepostiory){
+        this.employeeRepostiory = employeeRepostiory;
+    }
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepostiory.findAll();
+    }
+
+    public Employee getEmployee(Long employeeId){
+
+        Employee employee = employeeRepostiory.getById(employeeId);
+
+        return employee;
+
+    }
+    public Employee createEmployee(Employee employeeObj){
+        return employeeRepostiory.save(employeeObj);
+    }
+
+
 }
