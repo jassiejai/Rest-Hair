2181ff1df5ad33dbc810b65a9ddbf92bff696bbc
Jassie <jasminehart94@gmail.com>
2022-03-29 15:25:15 -0700

    Creating Controller

--- src/main/java/com/example/resthair/controller/EmployeeController.java
+++ src/main/java/com/example/resthair/controller/EmployeeController.java
@@ -1,4 +1,40 @@
 package com.example.resthair.controller;
 
+
+import com.example.resthair.model.Employee;
+import com.example.resthair.service.EmployeeService;
+import org.apache.catalina.LifecycleState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    private void setEmployeeService(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employee/{employeeId}")
+    public Employee getEmployee(@PathVariable Long employeeId){
+        return employeeService.getEmployee(employeeId);
+    }
+
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee employeeObj){
+        return employeeService.createEmployee(employeeObj);
+    }
+
+
 }
